22c64483ed1545b1f4fa29b6218961d625441aad
sorcho <mariobrosscot@yahoo.it>
2022-01-27 17:56:09 +0100

    added comments and requirements

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import random
-import qrcode
-from PIL import Image
+# In order to import every library needed run the following command "pip install -r requirements.txt"
 
-random_number_one = random.randint(1000,9999)
-random_number_two = random.randint(1000,9999)
+import random # Imported random library
+import qrcode # Imported qrcode library
+from PIL import Image # Imported Image library from PILLOW
 
-email = input("Inserisci la mail: ")
-month = input("Inserisci un mese (tra 01 e 12): ")
-day = input("Inserisci un giorno (tra 01 e 31): ")
+random_number_one = random.randint(1000,9999) # Generate first 4 digit string
+random_number_two = random.randint(1000,9999) # Generate second 4 digit string
 
-data = qrcode.make(str(random_number_one) + '|' + str(random_number_two) + '|2022-' + str(month) + '-' + str(day) + '|UNINA\\' + email)
+email = input("Inserisci la mail: ") # User writes his email
+month = input("Inserisci un mese (tra 01 e 12): ") # User writes the month
+day = input("Inserisci un giorno (tra 01 e 31): ") # User writes the day
 
+stringWrap = str(random_number_one) + '|' + str(random_number_two) + '|2022-' + str(month) + '-' + str(day) + '|UNINA\\' + email # Wraps every information into a single variable
+data = qrcode.make(stringWrap) # Creates the QRCode (I think at least)
+
+# From now on I just brutally copy-pasted everything
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -20,4 +24,4 @@ qr = qrcode.QRCode(
 
 qr.add_data(data)
 qr.make(fit=True)
-data.save("sample.png")
+data.save("sample.png") # Saves the QRCode as "sample.png"
